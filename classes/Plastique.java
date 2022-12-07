08aba6a6a09a5d8347bcedfc72577fe1c6dceaab
youya <hayamamlouk25@gmail.com>
2022-12-07 18:00:06 +0100

    added javadoc for class Plastique

--- classes/Plastique.java
+++ classes/Plastique.java
@@ -11,15 +11,27 @@
 public abstract class Plastique extends Ressource {
     private int age;
 
+    /**
+     * Constructeur qui initialise le type du plastique et sa quantité. Au moment de création,l'âge est 0 par défaut
+     * @param type type du plastique
+     * @param qte quantité du plastique
+     */
     public Plastique(String type, int qte) {
         super(type, qte);
         age = 0; // a l'instant du depot ou de crwation
     }
 
+    /**
+     * permet d'avoir acces a l'âge du plastique
+     * @return l'âge du plastqiue
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * augmente l'âge du plastique d'un an
+     */
     public void augmenteAge() {
         age++;
     }
